e18717095ff7a0401bd30199f1c944aacb1cf85e
pie2964 <131858014+pie2964@users.noreply.github.com>
2023-11-27 13:11:14 +0900

    URL 게시 기능 추가 및 description 성능 개선

--- frontend/src/components/EventItem.js
+++ frontend/src/components/EventItem.js
@@ -1,6 +1,15 @@
 import { Link, useRouteLoaderData, useSubmit } from "react-router-dom";
 import classes from "./EventItem.module.css";
 
+// url 길이 40자 이상 넘어가면 ...표현
+function truncateString(str, maxLength) {
+  if (str.length > maxLength) {
+    return str.substring(0, maxLength) + "...";
+  } else {
+    return str;
+  }
+}
+
 function EventItem({ event }) {
   const token = useRouteLoaderData("root");
   const submit = useSubmit();
@@ -19,6 +28,14 @@ function EventItem({ event }) {
       <h1>{event.title}</h1>
       <time>{event.date}</time>
       <p>{event.description}</p>
+      {event.url && (  // 이벤트에 URL이 있는 경우에만 표시
+        <p>
+          <label htmlFor="url"></label>
+          <a href={event.url} target="_blank" rel="noopener noreferrer">
+          {truncateString(event.url, 40)} 
+          </a>
+        </p>
+      )}
       {token && (
         <menu className={classes.actions}>
           <Link to="edit">수정하기</Link>
